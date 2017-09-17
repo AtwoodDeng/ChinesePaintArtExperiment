e0486d9bd4d4b03338e1dc9b4869f7a9dc09cb51
Atwood Deng <dengjdtsinghua@gmail.com>
2017-09-17 14:50:08 -0700

    init

---
+++ Assets/Script/ImageEffectTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CinematicEffects;
+
+[ExecuteInEditMode]
+public class ImageEffectTest : MonoBehaviour {
+
+	[SerializeField] Shader m_shader;
+
+	private Material m_Material;
+	public Material material
+	{
+		get
+		{
+			if (m_Material == null)
+				m_Material = ImageEffectHelper.CheckShaderAndCreateMaterial(m_shader);
+
+			return m_Material;
+		}
+	}
+
+	void OnRenderImage( RenderTexture source , RenderTexture destination )
+	{
+
+		Graphics.Blit (source, destination, material);
+
+	}
+}
